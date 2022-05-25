31afa04ae14afd0b515e20b98e5aa0db46afe84a
Martin Braun <braun@neuroforge.de>
2022-05-25 20:32:21 +0000

    0.1.0

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ import unittest
 
 def cli_test_suite():
     test_loader = unittest.TestLoader()
-    test_suite = test_loader.discover('tests', pattern='test_*.py')
+    test_suite = test_loader.discover("tests", pattern="test_*.py")
     return test_suite
 
 
@@ -13,7 +13,7 @@ with open("README.md", "r", encoding="utf-8") as fh:
 
 setuptools.setup(
     name="docker-stack-deploy",
-    scripts=['bin/docker-sdp'],
+    scripts=["bin/docker-sdp"],
     version="0.1.0",
     author="NeuroForge GmbH & Co. KG",
     author_email="kontakt@neuroforge.de",
@@ -21,20 +21,15 @@ setuptools.setup(
     long_description=long_description,
     long_description_content_type="text/markdown",
     url="https://neuroforge.de/",
-    package_data={ "docker-stack-deploy": ["py.typed"] },
+    package_data={"docker-stack-deploy": ["py.typed"]},
     packages=setuptools.find_packages(),
     classifiers=[
         "Programming Language :: Python :: 3.8",
         "License :: OSI Approved :: MIT License",
         "Operating System :: POSIX :: Linux",
     ],
-    test_suite='setup.cli_test_suite',
-    python_requires='>=3.8',
-    install_requires=[
-        "pyyaml",
-        "mypy>=0.800"
-    ],
-    extras_require={
-        'dev': []
-    }
+    test_suite="setup.cli_test_suite",
+    python_requires=">=3.8",
+    install_requires=["pyyaml", "mypy>=0.800"],
+    extras_require={"dev": []},
 )
