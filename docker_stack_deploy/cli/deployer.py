c99c0cc355cf5d88e05d1dc200885ef54d684d78
Martin Braun <braun@neuroforge.de>
2023-05-27 14:08:02 +0200

    change import of collections to collections.abc

--- docker_stack_deploy/cli/deployer.py
+++ docker_stack_deploy/cli/deployer.py
@@ -6,7 +6,7 @@ import sys
 import os
 import hashlib
 import subprocess
-import collections
+import collections.abc as collections
 from copy import deepcopy
 
 VERBOSE: bool = (
@@ -251,7 +251,7 @@ def docker_stack_deploy() -> None:
 def usage() -> None:
     print(
         """
-docker-stack-deploy (docker-sdp) v0.2.7
+docker-stack-deploy (docker-sdp) v0.2.11
 =======================================
 
 docker-stack-deploy (docker-sdp) is a utility that wraps around dockers to but adds the following features:
